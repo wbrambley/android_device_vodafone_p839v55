449026ade7bf142ee657fb48cf99ea02b8f10818
John Bennett <nutsda@gmail.com>
2017-08-31 20:45:20 +1200

    NFC related

--- device.mk
+++ device.mk
@@ -199,16 +199,20 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
     NfcNci \
     Tag \
-    nfc_nci.pn54x.default
+    nfc_nci.pn54x.default \
+    com.android.nfc_extras
 
-PRODUC_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 # Power HAL
 PRODUCT_PACKAGES += \
