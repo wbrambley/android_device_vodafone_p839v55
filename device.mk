3777de2dd33a238bfc3e234eae1a3bb88b9318f1
John Bennett <nutsda@gmail.com>
2017-09-11 19:26:29 +1200

    Add ANT Radio permissions

--- device.mk
+++ device.mk
@@ -79,6 +79,9 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
